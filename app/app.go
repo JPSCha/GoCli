3a108dbc75d636c246a4c88ef19d17809126e4ea
JPSCha <joaopaulosuskichaves@gmail.com>
2024-09-02 21:31:22 -0300

    Finished Go cli app

--- app/app.go
+++ app/app.go
@@ -14,20 +14,29 @@ func Generate() *cli.App {
 	app.Name = "Gli"
 	app.Usage = "Search IPs and server names on the web"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+			Usage: "host to search",
+			Value: "",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
 			Name: "ip",
 			Usage: "Search for IP adresses on the web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "host",
-					Usage: "host to search",
-					Value: "www.google.com",
-				},
-			},
+			Flags: flags,
 
 			Action: SearchIp,
 
+		},
+		{
+			Name: "servers",
+			Usage: "Search for server names on the web",
+			Flags: flags,
+
+			Action: SearchServers,
 		},
 	}
 
@@ -44,5 +53,18 @@ func SearchIp(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func SearchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host) //NS = name server
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
